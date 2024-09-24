8176c34b03cf8daf574b1091eddea9b693f5afde
NStor <andyr@mail.ru>
2024-09-24 21:46:35 +0300

    update repository

--- install_comfy_cli.bat
+++ install_comfy_cli.bat
@@ -8,10 +8,10 @@ echo Creating "Virtual environmen"
 "%python%" -m venv "%CD%\venv"
 echo done
 echo ==========================================================================
-echo Upgrade "pip"
-"%CD%\venv\Scripts\python" -m pip install --upgrade pip
-echo done
-echo ==========================================================================
+::echo Upgrade "pip"
+::"%CD%\venv\Scripts\python" -m pip install --upgrade pip
+::echo done
+::echo ==========================================================================
 echo Installing "comfy-cli"
 "%CD%\venv\Scripts\python" -m pip install comfy-cli
 echo done
@@ -20,6 +20,10 @@ echo Installing "ComfyUI" with "Manager"
 "%CD%\venv\Scripts\comfy" --here --skip-prompt install --nvidia
 echo done
 echo ==========================================================================
+:: Uncomment 2 next command, if you need to start dowloading first model-file
+::echo Downloading first model
+::"%CD%\venv\Scripts\comfy" --skip-prompt model download --url https://civitai.com/api/download/models/128713
+::echo ==========================================================================
 echo Starting "ComfyUI"
 "%CD%\venv\Scripts\comfy" launch -- --windows-standalone-build
 echo ==========================================================================
